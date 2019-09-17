4528742cabbb946ce586b927cb84bdd2f38931e7
june <june.@qq.com>
2019-09-17 18:20:32 +0800

    封装函数

--- index.js
+++ index.js
@@ -1,57 +1,21 @@
-		var love=document.getElementById("love")
-		var num=document.getElementById("num")
-		var love1=document.getElementById("love1")
-		var num1=document.getElementById("num1")
-		var text=parseInt(num.innerHTML)
-		var text1=parseInt(num1.innerHTML)
-		var love2=document.getElementById("love2")
-		var num2=document.getElementById("num2")
-		var text2=parseInt(num2.innerHTML)
-		function fn(){
-			var value=0;
-			return function info(){
+		function $(id){
+			return document.getElementById(id)
+		}
+		function fn(love,num){			
+			 var value=num.innerHTML;
 				if(value % 2==0){
 				value++;
-				num.innerHTML=text+value
+				num.innerHTML=value
 				love.style.backgroundColor="red"
 			}else{
 					value--;
-					num.innerHTML=text-value
+					num.innerHTML=value
 					love.style.backgroundColor="white"
 			}
-			}
 		}
-		function fn1(){
-			var value=0;
-			return function info(){
-				if(value % 2==0){
-				value++;
-				num1.innerHTML=text1+value
-				love1.style.backgroundColor="red"
-			}else{
-					value--;
-					num1.innerHTML=text1-value
-					love1.style.backgroundColor="white"
-			}
-			}
-		}
-		function fn2(){
-			var value=0;
-			return function info(){
-				if(value % 2==0){
-				value++;
-				num2.innerHTML=text2+value
-				love2.style.backgroundColor="red"
-			}else{
-					value--;
-					num2.innerHTML=text2-value
-					love2.style.backgroundColor="white"
-			}
-			}
-		}
-		love.onclick=fn()
-		love1.onclick=fn1()
-		love2.onclick=fn2()
+		$("love").onclick=function() {fn($("love"),$("num"))}
+		$("love1").onclick=function() {fn($("love1"),$("num1"))}
+		$("love2").onclick=function() {fn($("love2"),$("num2"))}
 		var hidden=document.getElementsByClassName("comment-hidden")
 		var show=document.getElementsByClassName("comment-show")
 		var zhedie=document.getElementsByClassName("zhedie")
